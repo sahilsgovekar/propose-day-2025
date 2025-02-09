f7cd14db8442195abbf8620c34bb36e9bb13ac7c
sahil.govekar <sahilgovekarcoding@gmail.com>
2025-02-09 12:01:05 -0800

    proposeDatContext init Code

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('login-form');
+const codeInput = document.getElementById('code');
+const submitBtn = document.getElementById('submit-btn');
+const resultDiv = document.getElementById('result');
+
+const secretCode = 'love'; 
+const nextPageUrl = 'main.html'; 
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const userInput = codeInput.value.trim();
+	if (userInput === secretCode) {
+		resultDiv.innerHTML = 'Baby';
+		window.location.href = nextPageUrl; // Redirect to new page
+	} else {
+		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Sona';
+	}
+});
